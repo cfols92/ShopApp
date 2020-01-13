5dec06f3fce61c731e111b3156d678791054374b
Chris Folsom <cfols92@gmail.com>
2020-01-13 15:55:42 -0500

    initialized git repo for shop app

---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+const adminData = require('./admin');
+
+router.get('/',(req,res,next) => {
+    const products = adminData.products;
+
+    res.render('shop',{prods:products, docTitle: 'Shop'});
+});
+
+module.exports = router;
