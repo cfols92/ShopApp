3ef28ae5324797ea583dcfe2b4f5ad05c9b41d01
Chris Folsom <cfols92@gmail.com>
2020-01-14 20:30:10 -0500

    added new routes and controllers

--- controllers/products.js
+++ controllers/products.js
@@ -13,7 +13,7 @@ exports.getAddProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
-  res.redirect('/');
+  res.redirect('/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -21,7 +21,35 @@ exports.getProducts = (req, res, next) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/',
+      path: '/products',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
+
+exports.getCart = (req,res,next) => {
+  // import from file that contains everything that has been "added to cart"
+  res.render('shop/cart',{
+    pageTitle: 'Cart',
+    path: '/cart'
+  });
+};
+
+exports.getIndex = (req,res,next) => {
+  res.render('shop/index',{
+    pageTitle: 'Home Page',
+    path: '/'
+  })
+};
+
+exports.getAdminProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/admin/products',
       hasProducts: products.length > 0,
       activeShop: true,
       productCSS: true
--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,10 @@ const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', productsController.getIndex);
+
+router.get('/products', productsController.getProducts);
+
+router.get('/cart', productsController.getCart);
 
 module.exports = router;
